aacd3fb9dbdc061bea06ff7570938f8b7af0de0a
MAleemH <aleemhashmi4321@gmail.com>
2023-03-04 13:30:52 +0500

    navbar added with some content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import Navbar from "./components/navbar";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='nav-center'>
+                <h3>Redux Toolkit</h3>
+                <div className="nav-container">
+                    <CartIcon />
+                    <div className="amount-container">
+                        <p className='total-amount'>0</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
